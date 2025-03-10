96f949a9c012b0a9d9fae1d1f318650b487b7f7b
ezra-nacianceno <e.nacianceno@creativehope.co.jp>
2025-03-10 21:19:26 +0800

    Add tailwind classes

--- app/page.js
+++ app/page.js
@@ -1,3 +1,8 @@
 export default function Home() {
-  return <h1>Online Portfolio</h1>;
+  return (
+    <>
+      <h1 className="font-varela">Online Portfolio</h1>
+      <p className="text-rose-quartz">Hello everyone! What is up?</p>
+    </>
+  );
 }
