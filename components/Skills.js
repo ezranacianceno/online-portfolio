b51772bacbf1d86d8721f5973e677b7eb225efb6
ezra-nacianceno <e.nacianceno@creativehope.co.jp>
2025-03-14 22:53:27 +0800

    Make Skills section more semantic and responsive

--- components/Skills.js
+++ components/Skills.js
@@ -2,27 +2,31 @@ export default function Skills() {
     return (
         <section id="skills" className="flex flex-col gap-4">
             <h2>Skills</h2>
-            <p className="font-sans leading-10 text-denim">
-                <span className="border rounded-full p-2 mr-3">React</span>
-                <span className="border rounded-full p-2 mr-3">Next JS</span>
-                <span className="border rounded-full p-2 mr-3">
-                    Tailwind CSS
-                </span>
-                <span className="border rounded-full p-2 mr-3">Strapi</span>
-                <span className="border rounded-full p-2 mr-3">WordPress</span>
-            </p>
-            <p className="font-sans">
-                <span className="border rounded-full p-2 mr-3">HTML5</span>
-                <span className="border rounded-full p-2 mr-3">CSS3</span>
-                <span className="border rounded-full p-2 mr-3">JavaScript</span>
-                <span className="border rounded-full p-2 mr-3">Bootstrap</span>
-                <span className="border rounded-full p-2 mr-3">Shadcn</span>
-                <span className="border rounded-full p-2 mr-3">MySQL</span>
-                <span className="border rounded-full p-2 mr-3">MongoDB</span>
-                <span className="border rounded-full p-2 mr-3">HubSpot</span>
-            </p>
+            <ul className="flex font-sans flex-wrap gap-y-2 text-denim">
+                <li className="border rounded-full p-2 mr-3">React</li>
+                <li className="border rounded-full p-2 mr-3">Next JS</li>
+                <li className="border rounded-full p-2 mr-3">Tailwind CSS</li>
+                <li className="border rounded-full p-2 mr-3">Strapi</li>
+                <li className="border rounded-full p-2 mr-3">WordPress</li>
+            </ul>
+            <ul className="flex font-sans flex-wrap gap-y-2">
+                <li className="border rounded-full p-2 mr-3">HTML5</li>
+                <li className="border rounded-full p-2 mr-3">CSS3</li>
+                <li className="border rounded-full p-2 mr-3">JavaScript</li>
+                <li className="border rounded-full p-2 mr-3">Bootstrap</li>
+                <li className="border rounded-full p-2 mr-3">Shadcn</li>
+                <li className="border rounded-full p-2 mr-3">MySQL</li>
+                <li className="border rounded-full p-2 mr-3">MongoDB</li>
+                <li className="border rounded-full p-2 mr-3">HubSpot</li>
+            </ul>
 
-            <h3 className="">Tools</h3>
+            <h3>Tools</h3>
+            <ul className="flex flex-wrap gap-y-2">
+                <li className="border rounded-full p-2 mr-3">GitHub</li>
+                <li className="border rounded-full p-2 mr-3">Docker</li>
+                <li className="border rounded-full p-2 mr-3">Postman</li>
+                <li className="border rounded-full p-2 mr-3">Figma</li>
+            </ul>
         </section>
     );
 }
